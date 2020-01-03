69c1b9cef190e1c96d652b325ab6a5a2c29c1562
Aliaksei Mikitsik <mikitsik@gmail.com>
2020-01-03 19:49:46 +0300

    Added the following gem and models:
    - add cancancan gem
    - add ability, role, users_roles models

--- app/controllers/advertisements_controller.rb
+++ app/controllers/advertisements_controller.rb
@@ -1,5 +1,6 @@
 class AdvertisementsController < ApplicationController
   before_action :set_advertisement, only: [:show, :edit, :update, :destroy]
+  load_and_authorize_resource
 
   # GET /advertisements
   # GET /advertisements.json
