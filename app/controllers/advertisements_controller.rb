3a3447e190b2a6585f10029fe00c7f08bf335ef4
Aliaksei Mikitsik <mikitsik@gmail.com>
2020-01-04 12:49:29 +0300

    Add admin and user roles

--- app/controllers/advertisements_controller.rb
+++ app/controllers/advertisements_controller.rb
@@ -5,7 +5,7 @@ class AdvertisementsController < ApplicationController
   # GET /advertisements
   # GET /advertisements.json
   def index
-    @advertisements = Advertisement.all
+    redirect_to root_path
   end
 
   # GET /advertisements/1
