0eb320cb6d67610c6ddaa4f0f44660d71902b315
Aliaksei Mikitsik <mikitsik@gmail.com>
2020-01-11 22:38:22 +0300

    Add custom pagination without any gems

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,25 @@
 class HomeController < ApplicationController
 
+  AD_PER_PAGE = 5
+
   def index
-    @advertisements = Advertisement.all
+    @pages = Advertisement.count / AD_PER_PAGE
+    unless @pages.zero?
+      @pages += 1
+    end
+
+    @page = permitted_page
+    unless @page.zero?
+      @page -= 1
+    end
+
+    @advertisements = Advertisement.offset(@page * AD_PER_PAGE).limit(AD_PER_PAGE)
+  end
+
+  private
+
+  def permitted_page
+    page = params.fetch(:page, 0).to_i
+    page <= AD_PER_PAGE ? page : AD_PER_PAGE
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ user_list.each do |name, surname, email, password, role|
   User.create( name: name, surname: surname, email: email, password: password, role: role )
 end
 
-55.times do |number|
+23.times do |number|
   Advertisement.create( user_id: 2, title: "Lorem Ipsum #{number + 1}",
                         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sit amet aliquet enim, eu aliquet est. Proin fringilla fermentum scelerisque. Nulla maximus, ligula a laoreet convallis, erat erat interdum diam, ut hendrerit tellus lacus id mi. Suspendisse potenti. Sed condimentum turpis eu lorem faucibus, id pulvinar risus lacinia. Nam euismod consequat augue eget efficitur. Quisque dapibus lacus nunc, facilisis blandit diam congue sit amet. Nunc eget eleifend nunc. Aliquam metus odio, tempor vel vestibulum eget, dictum a ante. Integer felis mauris, tincidunt sit amet turpis a, lacinia pharetra magna. Pellentesque aliquet elementum tempus. Integer aliquet elit aliquam sem ultricies, id molestie nunc mollis. Nunc justo erat, posuere nec ornare eget, scelerisque nec lacus.')
 end
