4f4b078a95dbb5fe51dc0726199e91e99541a13a
Aliaksei Mikitsik <mikitsik@gmail.com>
2020-01-11 21:01:05 +0300

    Added users and ads seeds

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,5 @@ class HomeController < ApplicationController
 
   def index
     @advertisements = Advertisement.all
-    
-    render 'home/index'
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user_list = [
+  ['Aleks', 'Petrov', 'test1@mail.com', '111111', 'admin' ],
+  ['Ivan', 'Sidorov', 'test2@mail.com', '111111', 'user' ],
+  ['Sergey', 'Ivanov', 'test3@mail.com', '111111', 'user' ]
+]
+
+user_list.each do |name, surname, email, password, role|
+  User.create( name: name, surname: surname, email: email, password: password, role: role )
+end
+
+55.times do |number|
+  Advertisement.create( user_id: 2, title: "Lorem Ipsum #{number + 1}",
+                        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sit amet aliquet enim, eu aliquet est. Proin fringilla fermentum scelerisque. Nulla maximus, ligula a laoreet convallis, erat erat interdum diam, ut hendrerit tellus lacus id mi. Suspendisse potenti. Sed condimentum turpis eu lorem faucibus, id pulvinar risus lacinia. Nam euismod consequat augue eget efficitur. Quisque dapibus lacus nunc, facilisis blandit diam congue sit amet. Nunc eget eleifend nunc. Aliquam metus odio, tempor vel vestibulum eget, dictum a ante. Integer felis mauris, tincidunt sit amet turpis a, lacinia pharetra magna. Pellentesque aliquet elementum tempus. Integer aliquet elit aliquam sem ultricies, id molestie nunc mollis. Nunc justo erat, posuere nec ornare eget, scelerisque nec lacus.')
+end
