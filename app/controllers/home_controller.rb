5b1feab5d1a9c51ec4ff2b230e79a80fd6434462
Aliaksei Mikitsik <mikitsik@gmail.com>
2020-01-25 17:45:17 +0300

    Fixed n+1 query problems

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
 
   def index
-    @advertisements = Advertisement.published.page(params[:page])
+    @advertisements = Advertisement.published.includes([:user, :images_attachments]).with_attached_images.page(params[:page])
   end
 end
--- app/controllers/users/ad_statuses_controller.rb
+++ app/controllers/users/ad_statuses_controller.rb
@@ -2,7 +2,7 @@ class Users::AdStatusesController < ApplicationController
   
   def index
     @advertisements = Advertisement.where(user_id: params[:user_id],
-                                          state: params[:status]).page(params[:page])
+                                          state: params[:status]).includes([:images_attachments]).with_attached_images.page(params[:page])
   end
 
   def update
